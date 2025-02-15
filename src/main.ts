6f0d363472a512cbc09be8fe3c524607720338e8
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2025-02-15 12:43:56 -0300

    chore(swagger): adding documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version, name, description } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,23 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle(name)
+    .setDescription(description)
+    .setVersion(version)
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(3000);
 }
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -9,23 +9,35 @@ import {
   HttpStatus,
   UseGuards,
 } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { ReplyMessageDto } from './dto/reply-message.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@ApiTags('Messages')
 @Controller('messages')
 @UseGuards(JwtAuthGuard)
+@ApiBearerAuth('access-token')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new message' })
+  @ApiResponse({ status: 201, description: 'Message created successfully.' })
   async create(@Body() createMessageDto: CreateMessageDto) {
     return this.messagesService.createMessage(createMessageDto);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Updates the content of an existing message' })
+  @ApiResponse({ status: 200, description: 'Message updated successfully.' })
   async update(
     @Param('id') id: string,
     @Body() updateMessageDto: UpdateMessageDto,
@@ -35,11 +47,15 @@ export class MessagesController {
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: 'Delete a message' })
+  @ApiResponse({ status: 204, description: 'Message deleted successfully.' })
   async remove(@Param('id') id: string) {
     await this.messagesService.deleteMessage(id);
   }
 
   @Post(':id/reply')
+  @ApiOperation({ summary: 'Reply to an existing message' })
+  @ApiResponse({ status: 201, description: 'Response created successfully.' })
   async reply(
     @Param('id') id: string,
     @Body() replyMessageDto: ReplyMessageDto,
